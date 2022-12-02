9d24d6bd0cdbf8c17d489105273f1b0f101d20a9
SabrinaMouginot <s.mouginot@yahoo.fr>
2022-12-02 01:27:20 +0100

    ajout dun 2e fichier js avec une seule div avec le code de base et lautre fichier js permet de manipuler les div en js

---
+++ script2.js
@@ -0,0 +1,191 @@
+document.write("<div class=div1><button>Click here</button></br>Bonjour d'un autre fichier JS!<br></br></div></br><div class=div2>");
+
+document.write("<br>&nbsp;</br>");
+document.write("<button>Click here</button>");
+document.write("</br>");
+document.write("Bonjour d'un autre fichier JS!");
+//Ceci est un commentaire sur une seule ligne
+/*C'est un commentaire
+sur plusieurs lignes*/
+document.write("<br></br>");
+document.write("LA BASE");
+document.write("</br>");
+
+var X = 5;                                                                   
+document.write(X);
+document.write("<br>&nbsp;</br>");
+
+document.write(" J'UTILISE IF ELSE.");
+document.write("</br>");
+
+var num = 55;
+var floating_point_num = 5.5;
+var string = 'Hey there!';
+var string_2 = "hé là! 'C'est entre guillemets simples'"
+var boolean = true; var boolean_2 = false;
+if(num <= 6){
+    document.write(" num est inférieur à 6.");
+}else if(num>6){
+    document.write(" num est supérieur à 6.");
+}else{
+    document.write(" num n'existe pas.");
+}
+document.write("<br>&nbsp;</br>");
+
+document.write(" J'UTILISE SWITCH CASE. ");     
+document.write("</br>");
+
+switch(num){
+    case 70:
+        document.write(" excellent! ");
+        break;
+    case 55:
+        document.write(" génial ");
+        break;
+    case 45:
+        document.write(" Bien ");
+        break;
+    case 30:
+        document.write(" D'accord ");
+        break;
+    default:
+        document.write(" Faire mieux ");
+        break;
+}
+document.write("<br>&nbsp;</br>");
+
+document.write(" J'UTILISE UNE FOR LOOP. ");     
+document.write("</br>");
+
+var Y;
+for (Y=1;Y<6;Y++){
+    document.write( Y+"<br/>" );
+    // document.write("");
+}
+document.write("</div>"); //fermeture de la div2
+document.write("</br>");
+
+// ********************************************************************************************************************************************************************************************************************
+
+document.write("<div class=div3><button>Click here</button></br>Bonjour d'un autre fichier JS!<br></br></br><div class=div4>");
+        document.write("<br>&nbsp;</br>");
+        document.write("<button>Click here</button>");
+        document.write("</br>");
+        document.write("Bonjour d'un autre fichier JS!");
+        //Ceci est un commentaire sur une seule ligne
+        /*C'est un commentaire
+        sur plusieurs lignes*/
+        document.write("<br></br>");
+        document.write("LA BASE");
+        document.write("</br>");
+
+        var X = 5;                                                                   
+        document.write(X);
+        document.write("<br>&nbsp;</br>");
+
+        document.write(" J'UTILISE IF ELSE.");
+        document.write("</br>");
+
+        var num = 55;
+        var floating_point_num = 5.5;
+        var string = 'Hey there!';
+        var string_2 = "hé là! 'C'est entre guillemets simples'"
+        var boolean = true; var boolean_2 = false;
+        if(num <= 6){
+            document.write(" num est inférieur à 6.");
+        }else if(num>6){
+            document.write(" num est supérieur à 6.");
+        }else{
+            document.write(" num n'existe pas.");
+        }
+        document.write("<br>&nbsp;</br>");
+
+        document.write(" J'UTILISE SWITCH CASE. ");     
+        document.write("</br>");
+
+        switch(num){
+            case 70:
+                document.write(" excellent! ");
+                break;
+            case 55:
+                document.write(" génial ");
+                break;
+            case 45:
+                document.write(" Bien ");
+                break;
+            case 30:
+                document.write(" D'accord ");
+                break;
+            default:
+                document.write(" Faire mieux ");
+                break;
+        }
+        document.write("<br>&nbsp;</br>");
+
+        document.write(" J'UTILISE UNE FOR LOOP. ");     
+        document.write("</br>");
+
+        var Y;
+        for (Y=1;Y<6;Y++){
+            document.write( Y+"<br/>" );
+            // document.write("");
+        }
+
+        document.write("</div>")
+
+document.write("LA BASE");
+document.write("</br>");
+
+var X = 5;                                                                   
+document.write(X);
+document.write("<br>&nbsp;</br>");
+
+document.write(" J'UTILISE IF ELSE.");
+document.write("</br>");
+
+var num = 55;
+var floating_point_num = 5.5;
+var string = 'Hey there!';
+var string_2 = "hé là! 'C'est entre guillemets simples'"
+var boolean = true; var boolean_2 = false;
+if(num <= 6){
+    document.write(" num est inférieur à 6.");
+}else if(num>6){
+    document.write(" num est supérieur à 6.");
+}else{
+    document.write(" num n'existe pas.");
+}
+document.write("<br>&nbsp;</br>");
+
+document.write(" J'UTILISE SWITCH CASE. ");     
+document.write("</br>");
+
+switch(num){
+    case 70:
+        document.write(" excellent! ");
+        break;
+    case 55:
+        document.write(" génial ");
+        break;
+    case 45:
+        document.write(" Bien ");
+        break;
+    case 30:
+        document.write(" D'accord ");
+        break;
+    default:
+        document.write(" Faire mieux ");
+        break;
+}
+document.write("<br>&nbsp;</br>");
+
+document.write(" J'UTILISE UNE FOR LOOP. ");     
+document.write("</br>");
+
+var Y;
+for (Y=1;Y<6;Y++){
+    document.write( Y+"<br/>" );
+    // document.write("");
+}
+
+document.write("</div>")
