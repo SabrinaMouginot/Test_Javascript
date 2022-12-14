8fb683020c194e03a55b6ba35c70e79f0aa177e5
SabrinaMouginot <s.mouginot@yahoo.fr>
2022-12-14 23:01:48 +0100

    les paramètres de fonction

--- script1.js
+++ script1.js
@@ -1,8 +1,26 @@
 document.write("<div class=TroisiemeFichier> </br> 3e FICHIER : LES FONCTIONS </div>");    
 
+document.write("<div class=div0>");
 document.write("<br>&nbsp;</br>");
+document.write(" APPELER UNE FONCTION");
+document.write("</br>");
 
 function imprimer(){
     document.write("Bonjour!")
 }
 imprimer()
+
+document.write("<br>&nbsp;</br>");
+document.write(" LES PARAMETRES DE FONCTION");
+document.write("</br>");
+
+function ajouter(a,b){
+    document.write(a+b)
+}
+ajouter(2,6)
+
+document.write("<br>&nbsp;</br>");
+document.write(" DECLARATION DE RETOUR");
+document.write("</br>");
+
+document.write("</div>"); // fin div0
