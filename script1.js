9e53a0478b43e29c3e2381a994653e3036e9424f
SabrinaMouginot <s.mouginot@yahoo.fr>
2022-12-14 23:40:39 +0100

    commentaires

--- script1.js
+++ script1.js
@@ -38,11 +38,12 @@ function ajout (c,d){
 var W = ajout(1,3);
 document.write(W);
 /******************************************************************************************************************************** */
-
+/**L'alerte permet de créer une noite de message pour informer l'utilisateur d'un évènement. */
 document.write("<br>&nbsp;</br>");
 document.write(" ALERTES");
 document.write("</br>");
 alert("Ceci est une alerte!");
+/**Une chaîne ou une variable de chaîne comme paramètre s'affiche comme message dans la boite d'alerte. */
 /******************************************************************************************************************************** */
 
 document.write("<br>&nbsp;</br>");
