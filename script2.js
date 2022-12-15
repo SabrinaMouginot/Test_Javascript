c2517bd199cc24af454f34cb9be34a722a8d6893
SabrinaMouginot <s.mouginot@yahoo.fr>
2022-12-15 06:02:40 +0100

    accéder aux paramètres et méthodes des objets

--- script2.js
+++ script2.js
@@ -1,32 +1,48 @@
 document.write("<div class=QuatrièmeFichier> </br> 4e FICHIER : OBJETS </div>");
-/**Un objet est une collection de variables stockées dans un système de paires nom: valeur. */
-var voiture = {
-    Marque: "Peugeot", Modele:"206", Type:"Coupé", TailleMotor: 2.0
-}
-
-/********************************************************************************************************************************************* */
-
+document.write("<br>&nbsp;</br>");
+/** Un objet est une collection de variables stockées dans un système de paires nom: valeur. */
 /** Propriété de l'objet: Les paramètres d'objet (variables) sont accessibles comme suit. */
 var voiture = {
     Marque: "Renault", Modele:"Mégane", Type:"Coupé", TailleMotor: 1.4
-}
+};
 
 var V = voiture.Marque;
 var V = voiture['Marque'];
 
-/**Méthodes d'objet: Les méthodes d'objet sont accessibles comme suit. */
-nomObjet.nomMethode();
-/**Pour accéder aux paramètres ainsi qu'aux méthodes, l'opérateur point ('.') est très utile. */
+document.write(V)
+document.write("<br>&nbsp;</br>");
 
-/********************************************************************************************************************************************* */
+// /**Méthodes d'objet: Les méthodes d'objet sont accessibles comme suit. */
+// nomObjet.nomMethode();
+// /**Pour accéder aux paramètres ainsi qu'aux méthodes, l'opérateur point ('.') est très utile. */
 
-/** Création d'objets */
-var voiture = {
-    Marque: "Ford", Modele:"Mustang", Type:"Coupe", TailleMotor: 5.2
-}
+// /********************************************************************************************************************************************* */
 
-var voiture2 = new car("Chevy","Camaro","Convertible", 6.2);
-document.write(voiture2.Marque);
-document.write(voiture2.TailleMotor);
+// /** Création d'objets */
+// var voiture = {
+//     Marque: "Ford", Modele:"Mustang", Type:"Coupe", TailleMotor: 5.2
+// }
+
+// var voiture2 = new car("Chevy","Camaro","Convertible", 6.2);
+// // voiture2.Marque = "Chevy";
+// // voiture2.Modele = "Camaro";
+// // voiture2.Type = "Convertible";
+// // voiture2.TailleMotor = 6.2;
+// document.write(voiture2.Marque);
+// document.write(voiture2.TailleMotor);
 
 
+// var voiture = {
+//     Marque: 'Ford', 
+//     Modele:'Mustang', 
+//     Type:'Coupe', 
+//     TailleMotor: 5.2
+// };
+
+// var voiture = new voiture();
+// voiture2.Marque = 'Chevy';
+// voiture2.Modele = 'Camaro';
+// voiture2.Type = 'Convertible';
+// voiture2.TailleMotor = 6.2;
+// document.write(voiture2.Marque);
+// document.write(voiture2.TailleMotor);
