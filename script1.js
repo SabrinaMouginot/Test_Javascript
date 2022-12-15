d583fb1ec3aa553caf4afa53c51ed68770951d85
SabrinaMouginot <s.mouginot@yahoo.fr>
2022-12-15 01:50:24 +0100

    commentaires sur les fonctions

--- script1.js
+++ script1.js
@@ -1,5 +1,11 @@
 document.write("<div class=TroisiemeFichier> </br> 3e FICHIER : LES FONCTIONS </div>");    
+/**Une fonction est un morceau de code conçu pour effectuer une tâche particulière ou être réutilisé encore et encore.
+ Les fonctions aident à réduire la longueur du code et à améliorer la répétabilité du code.
+ */
 
+/**Une fonction peut-être appelée à partir de n'importe quelle partie du programme pour démarrer son éxecution.
+ Pour appeler une fonction, spécifiez le nom de la fonction suivi d'un ensemble de parenthèses.
+ */
 document.write("<div class=div0>");
 document.write("<br>&nbsp;</br>");
 document.write(" APPELER UNE FONCTION");
@@ -9,8 +15,11 @@ function imprimer(){
     document.write("Bonjour!")
 }
 imprimer()
+/**Ici, la fonction 'imprimer' est appelée en mentionnant son nom puis en le suivant par un sensemble de parenthèses (). */
+
 /******************************************************************************************************************************** */
 
+/**Les valeurs peuvent être envoyées à une fonction en tant qu'attibuts. Ceux-ci peuvent ensuite être exploités par une fonction. */
 document.write("<br>&nbsp;</br>");
 document.write(" LES PARAMETRES DE FONCTION");
 document.write("</br>");
@@ -22,8 +31,14 @@ function ajouter(a,b){
     document.write(a+b)
 }
 ajouter(2,6)
+/**Ici, la fonction 'ajouter' est appelée et 2 valeurs lui sont envoyées. 
+ Ces valeurs sont ajoutées et imprimées par la fonction.
+ Une fonction peut prendre n'importe quel nombre de paramètres.
+ */
+
 /******************************************************************************************************************************** */
 
+/**Une fonction peut renvoyer une valeur à l'endroit où elle a été appelée à l'aide de l'instruction 'return'. */
 document.write("<br>&nbsp;</br>");
 document.write(" DECLARATION DE RETOUR");
 document.write("</br>");
@@ -37,6 +52,10 @@ function ajout (c,d){
 
 var W = ajout(1,3);
 document.write(W);
+/**Ici, les valeurs 1 et 3 sont envoyées à la fonction 'ajouter'.
+ Cette fonction ajoute ensuite ces deux valeurs et utilise l'instruction "return" à l'endroit où l'endroit où elle a été appelée.
+ Ici, la valeur retournée est affectée à la variable W. */
+
 /******************************************************************************************************************************** */
 
 /**L'alerte permet de créer une noite de message pour informer l'utilisateur d'un évènement. */
@@ -62,6 +81,7 @@ document.write("<br>&nbsp;</br>");
 
 /******************************************************************************************************************************** */
 
+/**La boite de confirmation est utilisée pour prendre la saisie de l'utilisateur lorsque quelque chose doit être confirmé ou accepté par l'utilisateur. */
 document.write("<br>&nbsp;</br>");
 document.write(" BOITE DE CONFIRMATION");
 document.write("</br>");
@@ -72,6 +92,8 @@ var option = confirm("Est-ce que tu t'appelles Harry?")
 if(option == True){
     document.write("salut harry");
 }
-
+/**La boîte de confirmation donne à l'utilisateur deux options - "ok" et "cancel". 
+ La fonction renvoie True, si l'utilisateur sélectionne "OK". 
+ La fonction renvoie False si l'utilisateur sélectionne "Cancel". */
 
 document.write("</div>"); // fin div0
