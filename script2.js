be3cb52a2b05a2aa0dbac07092b71726becac9d4
SabrinaMouginot <s.mouginot@yahoo.fr>
2022-12-15 03:39:41 +0100

    creation objets revoir

--- script2.js
+++ script2.js
@@ -1,16 +1,30 @@
 document.write("<div class=QuatrièmeFichier> </br> 3e FICHIER : OBJETS </div>");
-/**Un objet est une collection de variables stockées dans un système de paires nom: valeur. */
+// /**Un objet est une collection de variables stockées dans un système de paires nom: valeur. */
+// var voiture = {
+//     Marque: "Peugeot", Modèle:"206", Type:"Coupé", Taille_Motor: 2.0
+// }
+
+// /** Propriété de l'objet: Les paramètres d'objet (variables) sont accessibles comme suit. */
+// var voiture = {
+//     Marque: "Renault", Modèle:"Mégane", Type:"Coupé", Taille_Motor: 1.4
+// }
+
+// var V = voiture.Marque;
+// var V = voiture['Marque'];
+
+// /**Méthodes d'objet: Les méthodes d'objet sont accessibles comme suit. */
+// nomObjet.nomMéthode();
+// /**Pour accéder aux paramètres ainsi qu'aux méthodes, l'opérateur point ('.') est très utile. */
+
+
+
+/** Création d'objets */
 var voiture = {
-    Marque: "Peugeot", Modèle:"206", Type:"Coupé", Taille_Motor: 2.0
+    Marque: "Ford", Modèle:"Mustang", Type:"Coupe", Taille_Motor: 5.2
 }
 
-/** Propriété de l'objet: Les paramètres d'objet (variables) sont accessibles comme suit. */
-var voiture = {
-    Marque: "Renault", Modèle:"Mégane", Type:"Coupé", Taille_Motor: 5.2
-}
+var voiture_2 = new voiture("Chevy","Camaro","Convertible", 6.2);
+document.write(voiture_2.Marque);
+document.write(voiture_2.Taille_Motor);
 
-var V = voiture.Marque;
-var V = voiture['Marque'];
-/**Méthodes d'objet: Les méthodes d'objet sont accessibles comme suit. */
-nomObjet.nomMéthode();
-/**Pour accéder aux paramètres ainsi qu'aux méthodes, l'opérateur point ('.') est très utile. */
+
