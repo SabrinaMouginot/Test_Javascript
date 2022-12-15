8e76e1e2255acb36e279e5f775b0f184d65938a4
SabrinaMouginot <s.mouginot@yahoo.fr>
2022-12-15 04:42:07 +0100

    Les objets

--- script2.js
+++ script2.js
@@ -1,30 +1,32 @@
-document.write("<div class=QuatrièmeFichier> </br> 3e FICHIER : OBJETS </div>");
-// /**Un objet est une collection de variables stockées dans un système de paires nom: valeur. */
-// var voiture = {
-//     Marque: "Peugeot", Modèle:"206", Type:"Coupé", Taille_Motor: 2.0
-// }
+document.write("<div class=QuatrièmeFichier> </br> 4e FICHIER : OBJETS </div>");
+/**Un objet est une collection de variables stockées dans un système de paires nom: valeur. */
+var voiture = {
+    Marque: "Peugeot", Modele:"206", Type:"Coupé", TailleMotor: 2.0
+}
 
-// /** Propriété de l'objet: Les paramètres d'objet (variables) sont accessibles comme suit. */
-// var voiture = {
-//     Marque: "Renault", Modèle:"Mégane", Type:"Coupé", Taille_Motor: 1.4
-// }
+/********************************************************************************************************************************************* */
 
-// var V = voiture.Marque;
-// var V = voiture['Marque'];
+/** Propriété de l'objet: Les paramètres d'objet (variables) sont accessibles comme suit. */
+var voiture = {
+    Marque: "Renault", Modele:"Mégane", Type:"Coupé", TailleMotor: 1.4
+}
 
-// /**Méthodes d'objet: Les méthodes d'objet sont accessibles comme suit. */
-// nomObjet.nomMéthode();
-// /**Pour accéder aux paramètres ainsi qu'aux méthodes, l'opérateur point ('.') est très utile. */
+var V = voiture.Marque;
+var V = voiture['Marque'];
 
+/**Méthodes d'objet: Les méthodes d'objet sont accessibles comme suit. */
+nomObjet.nomMethode();
+/**Pour accéder aux paramètres ainsi qu'aux méthodes, l'opérateur point ('.') est très utile. */
 
+/********************************************************************************************************************************************* */
 
 /** Création d'objets */
 var voiture = {
-    Marque: "Ford", Modèle:"Mustang", Type:"Coupe", Taille_Motor: 5.2
+    Marque: "Ford", Modele:"Mustang", Type:"Coupe", TailleMotor: 5.2
 }
 
-var voiture_2 = new voiture("Chevy","Camaro","Convertible", 6.2);
-document.write(voiture_2.Marque);
-document.write(voiture_2.Taille_Motor);
+var voiture2 = new car("Chevy","Camaro","Convertible", 6.2);
+document.write(voiture2.Marque);
+document.write(voiture2.TailleMotor);
 
 
